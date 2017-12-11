1e65285d336cd714f713d4bd24b7fe510c177d24
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-12-12 00:37:40 +0100

    feat(grid): added back grid, made it toggleabe, & axes too

--- src/rendering/render.js
+++ src/rendering/render.js
@@ -1,5 +1,5 @@
 const makeDrawMesh = require('./drawMesh/index')
-// const makeDrawGrid = require('./drawGrid/index')
+const makeDrawGrid = require('./drawGrid/index')
 const renderWrapper = require('./renderWrapper')
 
 const makeDrawMeshNoNormals = require('./drawMeshNoNormals')
@@ -10,8 +10,8 @@ const prepareRender = (regl, params) => {
   const {geometry} = params
   const drawCSG = makeDrawMesh(regl, {geometry})
   // const drawGrid = prepDrawGrid(regl, {fadeOut: true, ticks: 10, size: [1000, 1000]})
-  //const drawNormals = makeDrawNormals(regl, {geometry})
-  /*const vectorizeText = require('vectorize-text')
+  // const drawNormals = makeDrawNormals(regl, {geometry})
+  /* const vectorizeText = require('vectorize-text')
   const complex = vectorizeText('Hello world! 你好', {
     triangles: true,
     width: 500,
@@ -30,10 +30,10 @@ const prepareRender = (regl, params) => {
 
   const drawTest = makeDrawMeshNoNormals(regl, {geometry: cube})
   const drawAxis = makeDrawAxis(regl, {})
-  // const drawGrid = makeDrawGrid(regl, {size: [1800, 1800], ticks: 10})  
+  const drawGrid = makeDrawGrid(regl, {size: [1800, 1800], ticks: 10})
 
   let command = (props) => {
-    const {meshColor, gridColor, background} = props
+    const {meshColor, background} = props
 
     renderWrapper(regl)(props, context => {
       regl.clear({
@@ -41,10 +41,15 @@ const prepareRender = (regl, params) => {
         depth: 1
       })
       drawCSG({color: meshColor})
-      // drawTest({color: [1, 0, 0, 1], model: mat4.translate(mat4.create(), mat4.identity([]), [100, 0, 200])})      
-      // drawGrid({color: gridColor})
-      drawAxis() // needs to be last to be 'on top' of the scene
-      //drawNormals()
+      // drawTest({color: [1, 0, 0, 1], model: mat4.translate(mat4.create(), mat4.identity([]), [100, 0, 200])})
+      if (props.grid.show) {
+        drawGrid({color: props.grid.color})
+      }
+      if (props.axes.show){
+        drawAxis() // needs to be last to be 'on top' of the scene
+      }
+      
+      // drawNormals()
     })
   }
   return function render (data) {
