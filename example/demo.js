15e869fd4e09a25334f8d9ddd9925fa380815388
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-12-23 13:20:04 +0100

    chore(demo): updated and cleaned up demo

--- example/demo.js
+++ example/demo.js
@@ -3,22 +3,20 @@ const {cube} = require('@jscad/scad-api').primitives3d
 const {color} = require('@jscad/scad-api').color
 
 const initializeData = function () {
-  return cube({size: 100})
+  return cube({size: 100 * Math.random()})
 }
 // dark bg : [0.211, 0.2, 0.207, 1]
 // dark grid :  [1, 1, 1, 0.1],
 
 const viewerOptions = {
-  singleton: true, // ensures that no matter how many times you call the creator function, you still only get a single instance  
   background: [0.211, 0.2, 0.207, 1], // [1, 1, 1, 1],//54, 51, 53
   meshColor: [0.4, 0.6, 0.5, 1],
   grid: {
+    display: true,
     color: [1, 1, 1, 0.1]
   },
   camera: {
-    position: [450, 550, 700],
-    target: [0, 0, 0],
-    far: 18000
+    position: [450, 550, 700]
   },
   controls: {
     zoomToFit: {
@@ -32,19 +30,35 @@ const viewerOptions = {
 }
 
 const csg = initializeData()
-const csgViewer = makeCsgViewer(document.body, viewerOptions)
-csgViewer(viewerOptions, {csg})
+const {csgViewer, viewerDefaults, viewerState$} = makeCsgViewer(document.body, viewerOptions)
 
+// update / initialize the viewer with some data
+csgViewer(viewerOptions, {solids: csg})
+
+// you also have access to the defaults
+console.log('viewerDefaults', viewerDefaults)
+
+// you can subscribe to the state of the viewer to react to it if you want to,
+// as the state is a most.js observable
+viewerState$
+  .throttle(5000)
+  // .skipRepeats()
+  .forEach(viewerState => console.log('viewerState', viewerState))
+
+// you can change the state of the viewer at any time by just calling the viewer
+// function again with different params
+// NOTE: the params need to respect the SAME structure as the defaults
 setTimeout(function (t) {
-  csgViewer({controls: {zoomToFit: 'all'}})
-}, 2000)
-/*
+  csgViewer({camera: { position: [0, 100, 100] }})
+}, 5000)
+
+// or different params AND different data
 setTimeout(function (t) {
   const csg = initializeData()
-  csgViewer({controls: {zoomToFit: 'none'}, meshColor: [1, 0, 0, 1], background: [0.2, 1, 1, 1]}, {csg})
-}, 4000) */
-/* setInterval(function (t) {
-  const csg = initializeData()
-  // console.log('a new one')
-  csgViewer(document.body, csg, viewerOptions)
-}, 2000) */
+  csgViewer({overrideOriginalColors: true, meshColor: [0.8, 0, 0, 1], background: [0.2, 1, 1, 1]}, {solids: csg})
+}, 10000)
+
+// and again
+setTimeout(function (t) {
+  csgViewer({controls: {autoRotate: {enabled: true}}})
+}, 15000)
