af84c7f1a29ea8f091004e204f0b37a99cf29e3f
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-01-02 20:38:50 +0100

    chore(demo): updated params to match new structure

--- example/demo.js
+++ example/demo.js
@@ -9,8 +9,10 @@ const initializeData = function () {
 // dark grid :  [1, 1, 1, 0.1],
 
 const viewerOptions = {
-  background: [0.211, 0.2, 0.207, 1], // [1, 1, 1, 1],//54, 51, 53
-  meshColor: [0.4, 0.6, 0.5, 1],
+  rendering: {
+    background: [0.211, 0.2, 0.207, 1], // [1, 1, 1, 1],//54, 51, 53
+    meshColor: [0.4, 0.6, 0.5, 1]
+  },
   grid: {
     display: true,
     color: [1, 1, 1, 0.1]
@@ -55,7 +57,7 @@ setTimeout(function (t) {
 // or different params AND different data
 setTimeout(function (t) {
   const csg = initializeData()
-  csgViewer({overrideOriginalColors: true, meshColor: [0.8, 0, 0, 1], background: [0.2, 1, 1, 1]}, {solids: csg})
+  csgViewer({overrideOriginalColors: true, rendering: {meshColor: [0.8, 0, 0, 1], background: [0.2, 1, 1, 1]}}, {solids: csg})
 }, 10000)
 
 // and again
