6f7d76e6ea265a9c58e21226c4c8962691d14056
Mark Moissette <kaosat.dev@gmail.com>
2018-01-31 15:04:09 +0100

    feat(lightColor): exposed lightColor (#32)
    * switched to a more neutral/not tinted default
    * closes #31

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ const makeCsgViewer = function (element, options = {}, inputs$ = most.never()) {
     rendering: {
       background: [1, 1, 1, 1],
       meshColor: [1, 0.5, 0.5, 1], // use as default face color for csgs, color for cags
+      lightColor: [1, 1, 1, 1],
       lightDirection: [0.2, 0.2, 1],
       lightPosition: [100, 200, 100],
       ambientLightAmount: 0.3,
